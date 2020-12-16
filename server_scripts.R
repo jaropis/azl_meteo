1dcfcf58e6c7f1ccf7211a58636a862c3684d6a6
Jarosław Piskorski <jaropis@zg.home.pl>
2020-12-16 23:25:51 +0100

    server script

---
+++ server_scripts.R
@@ -0,0 +1,6 @@
+prepare_weather <- function() {
+  data <- readRDS('meteo.RDS')
+  pressure <- list(x = data$now, y = data$pressure)
+  jsonlite::toJSON(pressure)
+  jsonlite::write_json(pressure, "pressure.json")
+}
